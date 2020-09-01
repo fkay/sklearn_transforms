f893c2e4f3f899285ff4fa1471c4403cebf8a1e1
Fabricio Kassardjian <fkay13@hotmail.com>
2020-08-31 23:52:51 -0300

    Transformacao por regressao linear

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -14,3 +14,34 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+
+# All sklearn Transforms must have the `transform` and `fit` methods
+class RegLinInputer(BaseEstimator, TransformerMixin):
+    def __init__(self, features, target):
+        self.features = features
+        self.target = target
+
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        data = X.query('{0} == {0}'.format(self.target))
+        print(data.shape)
+                
+        Xrl = data[self.features]
+        print(Xrl.head())
+
+        yrl = data[self.target]
+        print(yrl.head())
+
+        reg = LinearRegression()
+        reg.fit(Xrl, yrl)
+                       
+                       
+        # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        data = X.copy()
+                       
+        data.loc[X[self.target].isnull(), self.target] = reg.predict(X[self.features])[X[self.target].isnull()]
+                               
+        # Retornamos um novo dataframe com valores preenchidos
+        return data
